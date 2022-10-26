1b1433df892ba8347aa46acd1d8b6795465825e5
Chris Cockcroft <chris.cockcroft@rovio.com>
2022-10-26 12:43:43 -0400

    Weapons don't scale with WeaponController scale (allow scaling to affect slot radius only)

--- Assets/_Project/Scripts/Game/Weapons/WeaponViewController.cs
+++ Assets/_Project/Scripts/Game/Weapons/WeaponViewController.cs
@@ -1,14 +1,23 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Project.Game.Weapons
 {
     public class WeaponViewController : MonoBehaviour
     {
         [field: SerializeField] public SpriteRenderer SpriteRenderer { get; private set; }
-        
+
+        private Transform _transform;
+
+        private void Awake()
+        {
+            _transform = transform;
+        }
+
         public void SetSlot(WeaponSlot weaponSlot)
         {
-            transform.SetParent(weaponSlot.Transform, false);
+            _transform.SetParent(weaponSlot.Transform, true);
+            _transform.localPosition = Vector3.zero;
             SetSortingOrder(weaponSlot.SortingOrder);
         }
 
