855c09dcb36561fef6d34e4da95969da46f13000
Chris Cockcroft <chris.cockcroft@rovio.com>
2022-11-29 15:32:58 -0500

    Initial work on Toxic enemy (drops static projectile on death)

--- Assets/_Project/Scripts/Game/Enemies/EnemyActionBase.cs
+++ Assets/_Project/Scripts/Game/Enemies/EnemyActionBase.cs
@@ -8,7 +8,6 @@ namespace Project.Game.Enemies
 {
     public class EnemyActionBase : MonoBehaviour
     {
-        [field: SerializeField] public float Cooldown { get; protected set; }
         [field: SerializeField] public bool CanInterrupt { get; protected set; }
 
         public void Interrupt()
