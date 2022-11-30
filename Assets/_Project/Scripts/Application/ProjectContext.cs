254c967a74f5de83e7a9037f31990e088b8b657d
Chris Cockcroft <chris.cockcroft@rovio.com>
2022-11-30 14:13:37 -0500

    Beacon removed for playtest

--- Assets/_Project/Scripts/Application/ProjectContext.cs
+++ Assets/_Project/Scripts/Application/ProjectContext.cs
@@ -26,13 +26,13 @@ namespace Project.Application
         {
             UnityEngine.Application.targetFrameRate = 60;
             
-            var beaconWrapper = GetComponentInChildren<BeaconWrapper>();
+            /*var beaconWrapper = GetComponentInChildren<BeaconWrapper>();
             Bind<IBonfire>(beaconWrapper);
 
             var analyticsManager = new AnalyticsManager();
             Bind<IAnalyticsProvider>(analyticsManager);
             analyticsManager.Register(beaconWrapper);
-            analyticsManager.Register(new GameAnalyticsWrapper());
+            analyticsManager.Register(new GameAnalyticsWrapper());*/
             
             Bind(levelDatabase);
             Bind(sceneLoader);
