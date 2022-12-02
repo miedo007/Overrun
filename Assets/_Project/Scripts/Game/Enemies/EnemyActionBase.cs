dd1d3d5e97b85ae22f5442e1242e546566381ceb
Chris Cockcroft <chris.cockcroft@rovio.com>
2022-12-02 09:13:09 -0500

    Activate feedback for enemy actions

--- Assets/_Project/Scripts/Game/Enemies/EnemyActionBase.cs
+++ Assets/_Project/Scripts/Game/Enemies/EnemyActionBase.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using Project.Feedback;
 using Project.Game.Player;
 using Project.Game.Rooms;
 using UnityEngine;
@@ -9,6 +10,7 @@ namespace Project.Game.Enemies
     public class EnemyActionBase : MonoBehaviour
     {
         [field: SerializeField] public bool CanInterrupt { get; protected set; }
+        [field: SerializeField] public FeedbackData ActivateFeedback { get; protected set; }
 
         public void Interrupt()
         {
@@ -31,6 +33,10 @@ namespace Project.Game.Enemies
             float time,
             Action onCompleteCallback)
         {
+            if (ActivateFeedback != null)
+            {
+                ActivateFeedback.Play(transform.position, Quaternion.identity);
+            }
             OnPerform(enemy, player, roomManager, time, onCompleteCallback);
         }
         
