62271531397005751cc18441ec014ee1b9db49d6
Chris Cockcroft <chris.cockcroft@rovio.com>
2022-12-08 14:35:30 -0500

    Initial work on new item to spawn projectiles on ticket collect. Not currently in pool. Added heavy ranged enemy that spawns projectiles on death.

--- Assets/_Project/Scripts/Game/Enemies/EnemyActionRangedAttack.cs
+++ Assets/_Project/Scripts/Game/Enemies/EnemyActionRangedAttack.cs
@@ -10,6 +10,7 @@ namespace Project.Game.Enemies
     public class EnemyActionRangedAttack : EnemyActionBase
     {
         [field: SerializeField] public ProjectileData ProjectileData { get; private set; }
+        [field: SerializeField] public int ProjectileCount { get; private set; } = 1;
         [field: SerializeField] public Vector3 SpawnOffset { get; private set; }
         [field: SerializeField] public float DamageFactor { get; private set; } = 0.5f;
         [field: SerializeField] public bool IsStaticProjectile { get; private set; }
@@ -19,27 +20,49 @@ namespace Project.Game.Enemies
             Action onCompleteCallback)
         {
             var position = enemy.transform.position;
-            var rotation = Quaternion.identity;
-            
-            if (!IsStaticProjectile)
+            Quaternion rotation = Quaternion.identity;
+
+            if (ProjectileCount == 1)
             {
-                var targetPosition = player.transform.position;
-                // vector from this object towards the target location
-                var vectorToTarget = (targetPosition- position).normalized;
-                // rotate that vector by 90 degrees around the Z axis
-                var rotatedVectorToTarget = Quaternion.Euler(0, 0, 90) * vectorToTarget;
-                rotation = Quaternion.LookRotation(Vector3.forward, rotatedVectorToTarget);
+                if (!IsStaticProjectile)
+                {
+                    var targetPosition = player.transform.position;
+                    // vector from this object towards the target location
+                    var vectorToTarget = (targetPosition- position).normalized;
+                    // rotate that vector by 90 degrees around the Z axis
+                    var rotatedVectorToTarget = Quaternion.Euler(0, 0, 90) * vectorToTarget;
+                    rotation = Quaternion.LookRotation(Vector3.forward, rotatedVectorToTarget);
+                    
+                }
+                
+                var projectile = LeanPool.Spawn(ProjectileData.ProjectilePrefab, position + SpawnOffset, rotation);
+            
+                projectile.Initialize(
+                    ProjectileData,
+                    enemy.CurrentMeleeDamage * DamageFactor, 
+                    -1, 
+                    -1,
+                    0
+                );
             }
+            else
+            {
+                var angleDelta = 360f / ProjectileCount;
+                for (int i = 0; i < ProjectileCount; i++)
+                {
+                    
+                    rotation = Quaternion.Euler(0,0, angleDelta * i);
+                    var projectile = LeanPool.Spawn(ProjectileData.ProjectilePrefab, position + SpawnOffset, rotation);
             
-            var projectile = LeanPool.Spawn(ProjectileData.ProjectilePrefab, position + SpawnOffset, rotation);
-            
-            projectile.Initialize(
-                ProjectileData,
-                enemy.CurrentMeleeDamage * DamageFactor, 
-                -1, 
-                -1,
-                0
-            );
+                    projectile.Initialize(
+                        ProjectileData,
+                        enemy.CurrentMeleeDamage * DamageFactor, 
+                        -1, 
+                        -1,
+                        0
+                    );
+                }
+            }
             
             onCompleteCallback?.Invoke();
         }
--- Assets/_Project/Scripts/Game/Items/ItemBehaviourSpawnProjectilesInCircle.cs
+++ Assets/_Project/Scripts/Game/Items/ItemBehaviourSpawnProjectilesInCircle.cs
@@ -28,7 +28,7 @@ namespace Project.Game.Items
                     var heroInfo = InjectionContainer.Instance.Injector.Get<HeroRegistry>().ActiveHero;
                     _rangedStatInfo = heroInfo.GetStat(rangedStat);
                 }
-
+                Debug.Log($"Ranged Stat Info :: {_rangedStatInfo}");
                 return _rangedStatInfo;
             }
         }
@@ -43,6 +43,7 @@ namespace Project.Game.Items
                     _damageStatInfo = heroInfo.GetStat(damageStat);
                 }
 
+                Debug.Log($"Damage Stat Info :: {_damageStatInfo}");
                 return _damageStatInfo;
             }
         }
@@ -82,6 +83,7 @@ namespace Project.Game.Items
         public override void Cleanup()
         {
             _rangedStatInfo = null;
+            _damageStatInfo = null;
         }
     }
 }
