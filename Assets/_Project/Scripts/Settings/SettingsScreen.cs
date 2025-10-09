e1c89642d9e3c48272e1ae78366db8d41eeb16a9
Miedo <elmoussali.elmehdi@yahoo.fr>
2025-10-09 15:47:23 -0400

    setting fix

--- Assets/_Project/Scripts/Settings/SettingsScreen.cs
+++ Assets/_Project/Scripts/Settings/SettingsScreen.cs
@@ -114,7 +114,7 @@ public void SendEmailToSupport()
             // In editor, CrazySDK is never actually initialized
             return false;
 #else
-            return CrazySDK.Instance != null;
+            return CrazySDK.IsInitialized;
 #endif
         }
     }
